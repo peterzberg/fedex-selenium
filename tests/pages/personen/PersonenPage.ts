1d8c41121d822655071048e5cff940b10579d251
Maggi71 <marco.amrein@gmail.com>
2017-10-30 16:28:55 +0100

    Pages for Individuell

--- tests/pages/personen/PersonenPage.ts
+++ tests/pages/personen/PersonenPage.ts
@@ -3,9 +3,15 @@ import {NeuePersonComponent} from './NeuePersonComponent';
 
 export class PersonenPage {
 
+  individuellZusammenstellenButton = element(by.css('.test-configure-yourself > button:nth-child(1)'))
+
   get() : NeuePersonComponent {
     browser.get('https://www.css.ch/de/home/privatpersonen/krankenkasse/praemienrechner.html/personen');
     return new NeuePersonComponent();
+  }
+
+  individuellZusammenstellen(){
+    this.individuellZusammenstellenButton.click();
   }
 
 }
--- tests/test-spec.ts
+++ tests/test-spec.ts
@@ -1,5 +1,10 @@
 import {PersonenPage} from './pages/personen/PersonenPage';
 import {NeuePersonComponent} from './pages/personen/NeuePersonComponent';
+import {IndividuellPage} from './pages/individuell/IndividuellPage';
+import {Grundversicherung} from './pages/individuell/Grundversicherung';
+import {BudgetTyp} from './pages/individuell/BudgetTyp';
+import {SpitalTyp} from './pages/individuell/SpitalTyp';
+import {WeitereLeistungen}  from './pages/individuell/WeitereLeistungen'
 import {Geschlecht} from './pages/Geschlecht'
 import {browser, element, by} from 'protractor';
 
@@ -13,5 +18,12 @@ describe('KV Happy Case', () => {
     neuePerson.setVorname('Test');
     neuePerson.setPostleitzahl(6006, 'Luzern');
     neuePerson.speichern();
+    personenPage.individuellZusammenstellen();
+    let individuellPage = new IndividuellPage();
+    individuellPage.setzeGrundversicherung(Grundversicherung.STANDARD);
+    //individuellPage.setzeBudgetTyp(BudgetTyp.SPARSAM);
+    individuellPage.setzeSpitalTyp(SpitalTyp.HALBPRIVAT);
+    individuellPage.setzeWeitereLeistungen([WeitereLeistungen.ALTERNATIV, WeitereLeistungen.FITNESS])
+    browser.sleep(10000);
   });
 });
