910b381a3036093101430c78dc5c39fed4d92e3a
Peter Zberg <peter.zberg@gmail.com>
2017-10-30 23:48:21 +0100

    configurable configuration

--- tests/conf.ts
+++ tests/conf.ts
@@ -2,6 +2,7 @@ import {Config} from 'protractor';
 
 let host:string = process.env.seleniumHost ? process.env.seleniumHost : 'localhost';
 let port:string = process.env.seleniumPort ? process.env.seleniumPort : '4444'
+let direct:boolean = process.env.seleniumDirectConnect ? process.env.seleniumDirectConnect === 'true' : true;
 
 export let config: Config = {
   framework: 'jasmine',
@@ -11,8 +12,8 @@ export let config: Config = {
   },
   specs: [ 'test-spec.js' ],
   seleniumAddress: 'http://' + host + ':' + port + '/wd/hub',
-  directConnect: false,
-  SELENIUM_PROMISE_MANAGER: true,
+  directConnect: direct,
+  SELENIUM_PROMISE_MANAGER: false,
   // You could set no globals to true to avoid jQuery '$' and protractor '$'
   // collisions on the global namespace.
   noGlobals: true
